0082aedcfa7bb762d1589fd749b14f4cb621fcb1
jacob <jvnguyen@ualberta.ca>
2022-10-08 22:24:31 -0600

    updated main to create a display

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-class Pong:
-    def __init__(self):
-        pass
+import pygame
+
+def main():
+    "Width and height for our pygame display"
+    width = 500
+    height = 400
+
+    pygame.init()
+    pygame.display.set_mode((width, height))
+    pygame.display.set_caption('Pong')
+    surface = pygame.display.get_surface()
+    game = Game(surface)
