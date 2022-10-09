6994d82503e6f872b2c471803da80edcc2e9e56c
jacob <jvnguyen@ualberta.ca>
2022-10-08 22:54:58 -0600

    updated game class to draw the ball to the middle of the screen

--- game.py
+++ game.py
@@ -1,4 +1,6 @@
 import pygame
+from ball import Ball
+import random
 
 class Game:
     def __init__(self, surface):
@@ -8,6 +10,24 @@ class Game:
         self.gameClock = pygame.time.Clock()
         self.closeClicked = False
         self.continueGame = True
+
+        "Width and height of our surface"
+        width = self.surface.get_width()
+        height = self.surface.get_height()
+
+        "Game colors"
+        colorWhite = 'white'
+
+        "sizes for objects in our game"
+        ballRadius = 5
+
+        "Determine the center of the screen"
+        centerX = width//2
+        centerY = height//2
+
+        "Generate a random velocity between the two numbers"
+        ballVelocity = random.randint(-5, 5)
+        self.ball = Ball(colorWhite, ballRadius, (centerX, centerY), ballVelocity, self.surface)
 
     def play(self):
         while not self.closeClicked:
@@ -23,5 +43,6 @@ class Game:
 
     def draw(self):
         self.surface.fill(self.bgColor)
+        self.ball.draw()
         pygame.display.update()
 
