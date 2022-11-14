e1f068feca3f9ccb7e61d5ee1e267d23b5fd71b9
jacob <jvnguyen@ualberta.ca>
2022-11-14 00:05:02 -0700

    added keep track of score, end the game if score is reached

--- game.py
+++ game.py
@@ -21,7 +21,7 @@ class Game:
         self.colorWhite = 'white'
 
         "game font size"
-        self.fontSize = 72
+        self.fontSize = 36
 
         "sizes for objects in our game"
         ballRadius = 5
@@ -47,6 +47,8 @@ class Game:
         self.ball = Ball(self.colorWhite, ballRadius, [centerX, centerY], ballVelocity, self.surface)
         self.leftPaddle = Paddle(self.colorWhite, leftX, top, boxWidth, boxHeight, paddleVelocity, self.surface)
         self.rightPaddle = Paddle(self.colorWhite, rightX, top, boxWidth, boxHeight, paddleVelocity, self.surface)
+        self.player_one_score = 0
+        self.player_two_score = 0
 
     def play(self):
         while not self.closeClicked:
@@ -76,16 +78,33 @@ class Game:
         self.ball.draw()
         self.leftPaddle.draw()
         self.rightPaddle.draw()
+        self.draw_score_1()
+        self.draw_score_2()
         pygame.display.update()
 
     def update(self):
+        end_score = 10
         self.ball.move()
         if self.leftPaddle.getRect().collidepoint(self.ball.getCenter()) or self.rightPaddle.getRect().collidepoint(
                 self.ball.getCenter()):
             self.ball.reverseVelocity()
+        if self.ball.center[0] > self.surface.get_width() - 5:
+            self.player_two_score += 1
+        if self.ball.center[0] < 5:
+            self.player_one_score +=1
+        if self.player_one_score == end_score or self.player_two_score == end_score:
+            self.continue_game = False
 
-    def drawScore(self, player):
+    def draw_score_1(self):
+        test_string = str(self.player_one_score)
         font = pygame.font.SysFont('', self.fontSize)
-        image = font.render(str(player), True, self.colorWhite)
+        image = font.render(test_string, True, self.colorWhite)
+        text_position = (self.surface.get_width() - image.get_width(), 0)
+        self.surface.blit(image, text_position)
 
-        self.surface.blit()
+    def draw_score_2(self):
+        test_string = str(self.player_two_score)
+        font = pygame.font.SysFont('', self.fontSize)
+        image = font.render(test_string, True, self.colorWhite)
+        text_position = (0, 0)
+        self.surface.blit(image, text_position)
